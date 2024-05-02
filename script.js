616242d04200454c5479a1bb246f755cc27465cd
Abinash Kalita <abinashkalita682@gmail.com>
2024-05-03 01:59:24 +0530

    improve nav animation

--- script.js
+++ script.js
@@ -6,38 +6,53 @@ navAnimation()
 
 function navAnimation() {
 
-    var tl = gsap.timeline({ paused: true })
+    var tl, isFinish = true
 
-    tl.to("nav .dropdown-bg ", {
-        height: 'calc(100vh - 65px)',
-        paddingBottom: '1rem',
-        duration: 0.3,
-    })
-    tl.to("nav .dropdownCont ", {
-        height: '55vh',
-        padding: 20,
-        duration: 0.4,
-        delay: 0.1
-    })
-    tl.from("nav .dropdownCont h1", {
-        y: 100,
-        duration: 0.3,
-        opacity: 0,
-        stagger: 0.1
-    })
+    function setTimeLine(elemID) {
 
-    $("nav .navLink.innerContent").mouseenter(() => { tl.play() })
+        tl = gsap.timeline({
+            paused: true,
+            onStart: () => { isFinish = false },
+            onReverseComplete: () => {
+                $('nav .navLink').removeClass('active')
+                isFinish = true
+            }
+        })
 
-    var menuClose_triggerElem = [$("nav .navLink:not(.innerContent)"), $("nav .contRight"), $('nav .dropdownCont')]
-    menuClose_triggerElem.forEach(function (elem, index) {
+        tl.to("nav .dropdown-bg ", {
+            height: 'calc(100vh - 65px)',
+            paddingBottom: '1rem',
+            duration: 0.3,
+        })
+
+        tl.to(`#${elemID} .dropdownCont`, {
+            maxHeight: '65vh',
+            padding: '1.5rem ',
+            duration: 0.3,
+            delay: 0.05
+        })
+        tl.to(`nav .dropdownCont h1`, {
+            y: 0,
+            duration: 0.3,
+            opacity: 1,
+            stagger: 0.05,
+
+        })
+    }
+
+
+    $("nav .navLink.dropdown").mouseenter(function () {
         
-        if (index == menuClose_triggerElem.length - 1) {
-            elem.mouseleave(() => { tl.reverse() })
+        if (!$(this).hasClass('active') && isFinish) {
+            $(this).addClass('active')
+            setTimeLine($(this).attr('id'))
+            tl.play()
         }
-        else {
-            elem.mouseenter(() => { tl.reverse() })
-        }
+    })
 
+    var menuClose_triggerElem = [$("nav .navLink:not(.dropdown)"), $("nav .contRight"), $('nav .dropdown-bg')]
+    menuClose_triggerElem.forEach(function (elem) {
+        elem.mouseenter(() => { tl.reverse() })
     })
 
 }
